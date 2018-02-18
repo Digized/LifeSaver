8189f2d2245f510b206bf445adf5cd515a36b4d7
Zuraiz Zafar <Zuraiz.zafar97@gmail.com>
2018-02-17 20:17:18 -0500

    home screen

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/phone")
+def phone():
+    return render_template("app.html", name="Zuraiz")
