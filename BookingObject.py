f05f6b084226eeb5d9582783f54221224b955221
Danyaal <danyaalsoni@gmail.com>
2018-02-18 02:22:30 -0500

    completed mongoDB databse using pymongo

--- BookingObject.py
+++ BookingObject.py
@@ -11,12 +11,13 @@ class BookingObject:
         self.bookingState='waiting'
         self.startTime=datetime.datetime.now()
         self.endTime=datetime.datetime.now()
-        client=MongoClient()
-        db=client.booking_object
-        # self.create_database()
+
+        self.create_database()
     def get_end_time(self):
         self.endTime= datetime.datetime.now()
         return self.endTime
+    def set_end_time(self, time):
+        endTime=time
     def get_patient(self):
         return self.patient
     def get_start_time(self):
@@ -40,43 +41,212 @@ class BookingObject:
     def set_booking_state(self,state):
         self.bookingState=state
     def create_database(self):
-        patient_i=patient_info(self.patient.firstName,self.patient.lastName,self.patient.healthCardId,self.patient.dateOfBirth,
-        self.patient.sex,self.patient.phoneNumber,self.patient.primaryAddress,self.patient.healthCondition).save()
-        booking=booking_object(patient_i,self.shortDescription,self.patientLocation,self.queueNumber,self.emergencyLevel,
-        self.notes,self.bookingState,self.startTime,self.endTime)
-        booking.save()
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        bookings=db.bookings
+        booking_data={
+            'name': self.patient.firstname,
+            'healthCardId': self.patient.healthCardId,
+            'expiryDate':self.patient.expiryDate,
+            'dateOfBirth':self.patient.dateOfBirth,
+            'sex':self.patient.sex,
+            'phoneNumber':self.patient.phoneNumber,
+            'primaryAddress':self.patient.primaryAddress,
+            'healthCondition':self.patient.healthCondition,
+            'description':self.shortDescription,
+            'queueNumber':self.queueNumber,
+            'emergencyLevel':self.emergencyLevel,
+            'patientLocation': self.patientLocation,
+            'notes':self.notes,
+            'bookingState':self.bookingState,
+            'startTime':self.startTime,
+            'endTime':self.endTime
+        }
+        result=bookings.insert_one(booking_data)
+        print('One post: {0}'.format(result.inserted_id))
     def getDatabase(self):
-        for book in booking_object.objects:
-            print(book.description)
-            print(book.patient_information.health_card_id)
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        bookings=db.bookings
+        for booking in bookings.find():
+            print(booking)
+    def updatePatientName(self,healthCardNum,newName):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'name':newName
+            }
+        }
+        )
+        # //print(result.matched_count)
+    def updatePatientDOB(self,healthCardNum,newDOB):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'dateOfBirth':newDOB
+            }
+        }
+        )
+    def updatePatientPhoneNumber(self,healthCardNum,newPhoneNum):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'phoneNumber':newPhoneNum
+            }
+        }
+        )
+    def updatePatientAddress(self,healthCardNum,newAddress):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'name':newName
+            }
+        }
+        )
+    def updatePatientHealthCondition(self,healthCardNum,newhealthCondition):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'healthCondition':newhealthCondition
+            }
+        }
+        )
+    def updateDescription(self,healthCardNum,newDescription):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'description':newDescription
+            }
+        }
+        )
+    def updatequeueNum(self,healthCardNum,newQueueNum):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'queueNumber':newQueueNum
+            }
+        }
+        )
+    def updateEmergencyLevel(self,healthCardNum,newEmergencyLevel):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'emergencyLevel':newEmergencyLevel
+            }
+        }
+        )
+    def updatePatientLocation(self,healthCardNum,newLocation):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'patientLocation':newLocation
+            }
+        }
+        )
+    def updateNotes(self,healthCardNum,newNotes):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'notes':newNotes
+            }
+        }
+        )
+    def updateBookingState(self,healthCardNum,newBookingState):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'bookingState':newBookingState
+            }
+        }
+        )
+    def updateEndTime(self,healthCardNum,newEndTime):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'endTime':newEndTime
+            }
+        }
+        )
+    def updatePatientSex(self,healthCardNum,newSex):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.update(
+        {'healthCardId':healthCardNum},
+        {
+            '$set': {
+                'sex':newSex
+            }
+        }
+        )
+    def deleteDocument(self,healthCardNum):
+        client=MongoClient()
+        db=client.booking_object_database
+        collection=db.booking_object_collection
+        result=db.bookings.delete_many({'healthCardId':healthCardNum})
     def deleteDatabases(self):
-        for book in booking_object.objects:
-            book.delete()
-    def editBookingInfoDatabase(self,healthCard,descript,queueNum,patientLoc,emergencyLvl,notes,booking_state):
-        for info in booking_object.objects(info.patient_information.health_card_id=healthCard):
-                info.update_one(set__queue_number=int(queueNum))
-                info.update_one(set__location=patientLoc)
-                info.update_one(set__emergency_level=int(emergencyLvl))
-                info.update_one(set__notes=str(notes))
-                info.update_one(set__booking_state=str(booking_state))
-                info.update_one(set__description=str(descript))
-                print('success')
-class patient_info(Document):
-    first_name=StringField()
-    last_name=StringField()
-    health_card_id=StringField()
-    date_of_birth=StringField()
-    sex=StringField()
-    phoneNumber=StringField()
-    primary_address=StringField()
-    health_condition=StringField()
-class booking_object(Document):
-    patient_information=ReferenceField(patient_info)
-    description=StringField()
-    location=GeoPointField()
-    queue_number=IntField()
-    emergency_level=IntField()
-    notes=StringField()
-    booking_state=StringField()
-    start_time=DateTimeField()
-    end_time=DateTimeField()
+         client=MongoClient()
+         db=client.booking_object_database
+         collection=db.booking_object_collection
+         db.bookings.drop()
+    #     for book in booking_object.objects:
+    #         book.delete()
+    # def editBookingInfoDatabase(self,healthCard,descript,queueNum,patientLoc,emergencyLvl,notes,booking_state):
+    #     for info in booking_object.objects(info.patient_information.health_card_id=healthCard):
+    #             info.update_one(set__queue_number=int(queueNum))
+    #             info.update_one(set__location=patientLoc)
+    #             info.update_one(set__emergency_level=int(emergencyLvl))
+    #             info.update_one(set__notes=str(notes))
+    #             info.update_one(set__booking_state=str(booking_state))
+    #             info.update_one(set__description=str(descript))
+    #             print('success')
--- testBookingObject.py
+++ testBookingObject.py
@@ -1,9 +1,11 @@
 from Patient import Patient
 from BookingObject import BookingObject
-p=Patient('9234444445','ddddankdndkjsnksjnk', '2','dob','sex','32323232','45mann','helth')
+p=Patient('111','2020', 'ddddankdndkjsnksjnk','dob','sex','32323232','45mann','helth')
 b=BookingObject(p,'is a yfdvdfvdfthing',1)
 # b.create_database()
+b.updatePatientName('333', "dsdsfeuirfhreiuhfryugfuyerddnew_name")
 b.getDatabase()
-b.editBookingInfoDatabase('1234444445','this is not a ornfeournfuoer descriptions iuerubirebgieurbgriebgir',3, (44,44), 2, 'dd', 'waiting')
+b.deleteDocument('333')
+# b.editBookingInfoDatabase('1234444445','this is not a ornfeournfuoer descriptions iuerubirebgieurbgriebgir',3, (44,44), 2, 'dd', 'waiting')
 # b.deleteDatabases()
 b.getDatabase()
